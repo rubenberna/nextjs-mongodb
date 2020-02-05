16e54448da7347cadf0fa536936911a43a723649
rubenberna <rubenmbernardes@gmail.com>
2020-02-05 17:26:28 +0100

    trial in zeit dynamic routing

--- pages/[...slug].js
+++ pages/[...slug].js
@@ -6,21 +6,30 @@ import SeoTable from '../components/table'
 
 const Complex = ({content, slug}) => (
   <Layout>
-    <Headline content={content}/>
-    <SeoTable slug={slug}/>
+    <h1>{slug}</h1>
   </Layout>
 )
+//
+// <Headline content={content}/>
+// <SeoTable slug={slug}/>
 
 /*
 Each Next.js page component allows us to fetch data server-side thanks to a function called getInitialProps. When this function is called, the initial page load is rendered server-side, which is great for SEO.
 */
 
+// Complex.getInitialProps = async ({asPath}) => {
+//   if ( asPath !== '/favicon.ico' ) {
+//     const urlContent = await axios.post('http://localhost:3000/api/content', {asPath})
+//     const { data } = urlContent
+//     return {
+//       content: data[0],
+//       slug: asPath
+//     }
+//   }
+// }
 Complex.getInitialProps = async ({asPath}) => {
   if ( asPath !== '/favicon.ico' ) {
-    const urlContent = await axios.post('http://localhost:3000/api/content', {asPath})
-    const { data } = urlContent
     return {
-      content: data[0],
       slug: asPath
     }
   }

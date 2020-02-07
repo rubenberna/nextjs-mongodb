409fc63dd7c2f0b4f9ad81b5d1ba7aa47374b4a3
rubenberna <rubenmbernardes@gmail.com>
2020-02-07 15:58:11 +0100

    renamed files

--- pages/[...slug].js
+++ pages/[...slug].js
@@ -4,7 +4,7 @@ import Layout from '../components/Layout'
 import Headline from '../components/headline'
 import SeoTable from '../components/table'
 
-const Complex = ({content, slug, origin, baseUrl}) => (
+const DymanicContent = ({content, slug, origin, baseUrl}) => (
   <Layout>
     <h1>{slug}</h1>
     <Headline content={content}/>
@@ -17,13 +17,13 @@ const Complex = ({content, slug, origin, baseUrl}) => (
 Each Next.js page component allows us to fetch data server-side thanks to a function called getInitialProps. When this function is called, the initial page load is rendered server-side, which is great for SEO.
 */
 
-Complex.getInitialProps = async (context) => {
+DymanicContent.getInitialProps = async (context) => {
   const { asPath } = context
   if ( asPath !== '/favicon.ico' ) {
     const { host } = context.req.headers
     const protocol = process.env.NODE_ENV === 'development' ? 'http://' : 'https://'
     const baseUrl = `${protocol}${host}`
-    const urlContent = await axios.post(`${baseUrl}/api/content`, {asPath})
+    const urlContent = await axios.post(`${baseUrl}/api/page_content`, {asPath})
     const { data } = urlContent
     return {
       content: data[0],
@@ -33,4 +33,4 @@ Complex.getInitialProps = async (context) => {
   }
 }
 
-export default Complex
+export default DymanicContent
